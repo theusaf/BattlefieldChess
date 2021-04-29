76bd8872a6975535cacd4a0d5974167cbd14b989
theusaf <theusafyt@gmail.com>
2021-04-29 12:37:46 -0700

    Initial commit

---
+++ src/org/theusaf/BattlefieldChess/ApplicationSketch.java
@@ -0,0 +1,25 @@
+package org.theusaf.BattlefieldChess;
+
+import processing.core.PApplet;
+
+public class ApplicationSketch extends PApplet {
+
+  public ApplicationSketch() {
+    // Constructor for setting up your instance variables
+  }
+
+  @Override
+  public void settings() {
+    // Code to set up settings, such as size, title, etc.
+  }
+
+  @Override
+  public void setup() {
+    // Code to set up your program, load images, etc.
+  }
+
+  @Override
+  public void draw(){
+    // Code to render objects
+  }
+}
---
+++ src/org/theusaf/BattlefieldChess/Main.java
@@ -0,0 +1,11 @@
+package org.theusaf.BattlefieldChess;
+
+import processing.core.PApplet;
+
+public class Main {
+
+  public static void main(String[] args) {
+    PApplet.main(args);
+  }
+
+}
