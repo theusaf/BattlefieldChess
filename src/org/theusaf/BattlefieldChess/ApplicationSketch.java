175eb131658ad96337db1f621758aedceb0cd6fa
theusaf <theusafyt@gmail.com>
2021-04-29 12:43:55 -0700

    Add libraries

--- src/org/theusaf/BattlefieldChess/ApplicationSketch.java
+++ src/org/theusaf/BattlefieldChess/ApplicationSketch.java
@@ -11,11 +11,13 @@ public class ApplicationSketch extends PApplet {
   @Override
   public void settings() {
     // Code to set up settings, such as size, title, etc.
+    size(800, 800, P3D);
   }
 
   @Override
   public void setup() {
     // Code to set up your program, load images, etc.
+    surface.setTitle("Battlefield Chess");
   }
 
   @Override
