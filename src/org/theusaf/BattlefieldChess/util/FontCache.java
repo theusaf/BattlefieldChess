77b3b8d97648b07090696d20f8b773dd6667a40b
theusaf <theusafyt@gmail.com>
2021-04-30 17:55:57 -0700

    Document FontCache.java

--- src/org/theusaf/BattlefieldChess/util/FontCache.java
+++ src/org/theusaf/BattlefieldChess/util/FontCache.java
@@ -5,6 +5,9 @@ import processing.core.PFont;
 
 import java.util.HashMap;
 
+/**
+ * Represents a cache for fonts.
+ */
 public class FontCache {
 
   private static final int FONT_SIZE = 64;
@@ -12,11 +15,23 @@ public class FontCache {
   private final PApplet SKETCH;
   private final HashMap<String, PFont> FONT_HASH_MAP;
 
+  /**
+   * Constructs a font cache.
+   *
+   * @param sketch The PApplet sketch to generate fonts with
+   */
   public FontCache(PApplet sketch) {
     SKETCH = sketch;
     FONT_HASH_MAP = new HashMap<>();
   }
 
+  /**
+   * Gets a font by name. If the font doesn't exist in the cache, it generates and caches a new font by that name.
+   * Fonts are set to size 64.
+   *
+   * @param key The name of the font
+   * @return The cached font
+   */
   public PFont getFont(String key) {
     PFont font = FONT_HASH_MAP.get(key);
     if (font == null) {
