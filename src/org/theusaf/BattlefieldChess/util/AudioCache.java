4520b1fa73bc7db5efef018d7bb0d2dc2caec7b5
theusaf <theusafyt@gmail.com>
2021-04-30 17:58:49 -0700

    Document AudioCache.java

--- src/org/theusaf/BattlefieldChess/util/AudioCache.java
+++ src/org/theusaf/BattlefieldChess/util/AudioCache.java
@@ -5,16 +5,31 @@ import processing.sound.SoundFile;
 
 import java.util.HashMap;
 
+/**
+ * Represents a cache for sounds.
+ */
 public class AudioCache {
 
   private final PApplet SKETCH;
   private final HashMap<String, SoundFile> SOUND_HASH_MAP;
 
+  /**
+   * Constructs an audio cache.
+   *
+   * @param sketch The sketch to generate the sound files upon
+   */
   public AudioCache(PApplet sketch) {
     SKETCH = sketch;
     SOUND_HASH_MAP = new HashMap<>();
   }
 
+  /**
+   * Gets the specified sound from the cache.
+   *
+   * @param key The path to the sound file.
+   * @return The cached sound. If the sound is not in the cache, returns <code>null</code>.
+   * The sound will then be asynchronously loaded and cached.
+   */
   public SoundFile getAudio(String key) {
     SoundFile sound = SOUND_HASH_MAP.get(key);
     if (sound == null) {
@@ -23,6 +38,11 @@ public class AudioCache {
     return sound;
   }
 
+  /**
+   * Removes audio from the cache.
+   *
+   * @param key The path to the audio file to remove from the cache
+   */
   public void removeAudio(String key) {
     SOUND_HASH_MAP.remove(key);
   }
